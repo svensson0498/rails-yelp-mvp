343d4f5f44dcc822ce857ecefb7d497a44de7389
Mikael Svensson <svensson_0498@hotmail.com>
2021-05-19 17:51:08 +0200

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  restaurant = Restaurant.create(
+  	name: Faker::Restaurant.name,
+  	address: Faker::Address.city,
+  	phone_number: Faker::PhoneNumber.phone_number,
+  	category: %w[chinse italian japanese french belgian].sample
+  	)
+  	restaurant.save
+end
